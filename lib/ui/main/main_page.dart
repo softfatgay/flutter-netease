5d3ad0f8b9a8afbfa8d5008a3a301658179b9e52
wantuan <wan_tuan@163.com>
2020-05-11 16:03:29 +0800

    ...

--- lib/ui/main/main_page.dart
+++ lib/ui/main/main_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter_app/ui/mine/mine.dart';
 import 'package:flutter_app/ui/shoping_market/shop_market.dart';
 import 'package:flutter_app/ui/shoping_market/web_view.dart';
 import 'package:flutter_app/ui/sort/sort.dart';
-import 'package:flutter_app/ui/topic/topic_page1.dart';
+import 'package:flutter_app/ui/topic/topic_page.dart';
 import 'package:flutter_app/widget/wrapper.dart';
 
 import '../home/home.dart';
