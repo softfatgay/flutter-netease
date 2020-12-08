02f10b0bf4dffcfb99fe2d32dad2e051fdd34492
wantuan <wan_tuan@163.com>
2020-12-08 22:42:19 +0800

    更新cookie

--- lib/utils/user_config.dart
+++ lib/utils/user_config.dart
@@ -5,8 +5,8 @@ const String user_icon_url = 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3
 
 // const String cookie ='yx_csrf=61f57b79a343933be0cb10aa37a51cc8; NTES_YD_SESS=CeSW42ks.2zHbRG87vm8zIICA6Iz26O2Tk1c.zRjbbU2JYRBJ0rMWQAa3mSPwzgyNUot7ky9t19g_sPefQ.FztFjefQgJZxUEtxTFQlvuxOtF9_xQk2bxRZJVe0syMAcjCrBL3xGbWLKBqtQrgTgVMSmCYedIbiww8fzdwKYU7RYDRwhNU2VYckSVJKddnEhim1Fd49owd8sWYhvaGCh18EjA17quaiQQEomqoyqIergf; P_INFO=17621577088|1603968997|1|yanxuan_web;';
 
-const String csrf_token = "9a887cb174353f0d91f9b3841be57f21";
-const String cookie = "NTES_YD_SESS=qoaRcpmY07N2078uPZjPt5qyjf4Cr5FO0TWdrixJHWiADJLIDjktuVixdMzT2hHeg0EYaQe4YO4HPwTcoVl8hY8FcoVHDAn05YnG8VBpXnrwoLDdZS63EHsDxiQF69y_MISyPaUUpfk2Qr.NSEiSPhWXOWAHVcEJsf7GUNqh02GBrZTdWwHto8ggM0R0oPnON9EoUMyEP7R_p_GBAhWs9KJt8jNHIjRkeMh_cnjhba_vo; P_INFO=17621577088|1606745769|1|yanxuan_web|00&99|null&null&null#shh&null#10#0|&0|null|17621577088; yx_csrf=9a887cb174353f0d91f9b3841be57f21;";
+const String csrf_token = "20adeea4e765034ae27552282ba462c4";
+const String cookie = "NTES_YD_SESS=LvWj2E9UamcN24P1sAi4iY0CZb2YZ2kAjxC5wo64MCofv4jcvh2dqAo653BxJIMGmzN98ZGO9sOMaYxUKAyuI9uWUKAMvf.zg9.nuAek_.wYKjv5DlXENMtv6oZWX1T73clTa8SSkp2JZwbVlNolaIC_sCfMAUN4tR1VafkJB_WRFDgiD4oH_IrTc7wicTDbGZf9DmhmEaoYoQJTa2WB9YNduhVMch02G3I7U.hIP87rK; P_INFO=17621577088|1607438133|1|yanxuan_web|00&99|null&null&null#shh&null#10#0|&0|null|1762157708f; yx_csrf=20adeea4e765034ae27552282ba462c4; ";
 //yx_csrf;NTES_SESS
 //yx_csrf;NTES_YD_SESS;P_INFO
 //flutter build apk --target-platform android-arm
