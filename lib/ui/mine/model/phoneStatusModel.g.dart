dfa1d20935df5aa515d5ce84ea78a6f1b3d30466
wantuan <wan_tuan@163.com>
2021-06-30 17:10:59 +0800

    model

---
+++ lib/ui/mine/model/phoneStatusModel.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'phoneStatusModel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PhoneStatusModel _$PhoneStatusModelFromJson(Map<String, dynamic> json) {
+  return PhoneStatusModel()
+    ..status = json['status'] as num
+    ..mobile = json['mobile'] as String
+    ..ucMobile = json['ucMobile'] as String
+    ..degrade = json['degrade'] as bool
+    ..mobileBindFlowControl = json['mobileBindFlowControl'] as bool
+    ..frequentlyAccount = json['frequentlyAccount'] as bool;
+}
+
+Map<String, dynamic> _$PhoneStatusModelToJson(PhoneStatusModel instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'mobile': instance.mobile,
+      'ucMobile': instance.ucMobile,
+      'degrade': instance.degrade,
+      'mobileBindFlowControl': instance.mobileBindFlowControl,
+      'frequentlyAccount': instance.frequentlyAccount,
+    };
