798f8d46c2b1235ba3dffe61df5b4efe17c3e845
wantuan <wan_tuan@163.com>
2021-07-19 17:39:13 +0800

    model

---
+++ lib/ui/goodsDetail/model/wapitemDeliveryModel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'wapitemDeliveryModel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WapitemDeliveryModel _$WapitemDeliveryModelFromJson(Map<String, dynamic> json) {
+  return WapitemDeliveryModel()
+    ..status = json['status'] as num
+    ..addressId = json['addressId'] as num
+    ..addressName = json['addressName'] as String
+    ..deliveryTime = json['deliveryTime'] as String
+    ..deliveryStatus = json['deliveryStatus'] as num;
+}
+
+Map<String, dynamic> _$WapitemDeliveryModelToJson(
+        WapitemDeliveryModel instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'addressId': instance.addressId,
+      'addressName': instance.addressName,
+      'deliveryTime': instance.deliveryTime,
+      'deliveryStatus': instance.deliveryStatus,
+    };
