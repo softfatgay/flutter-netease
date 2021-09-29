a9dfcf985d3dfeb019aa3f40df63ee6d91b504f4
wantuan <wan_tuan@163.com>
2021-09-29 15:42:37 +0800

    model

---
+++ lib/model/topLogo.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'topLogo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TopLogo _$TopLogoFromJson(Map<String, dynamic> json) {
+  return TopLogo()
+    ..logoUrl = json['logoUrl'] as String
+    ..width = json['width'] as num
+    ..height = json['height'] as num
+    ..type = json['type'] as num;
+}
+
+Map<String, dynamic> _$TopLogoToJson(TopLogo instance) => <String, dynamic>{
+      'logoUrl': instance.logoUrl,
+      'width': instance.width,
+      'height': instance.height,
+      'type': instance.type,
+    };
